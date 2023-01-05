8cdacbc2219f82d120a724d7067d72ed2436c8b1
Hayden Ball <hayden@playerdata.com>
2023-01-05 12:26:16 +0000

    test(rabbitmq): Add test back for delayed message exchange plugin

--- Formula/rabbitmq_delayed_message_exchange.rb
+++ Formula/rabbitmq_delayed_message_exchange.rb
@@ -14,4 +14,18 @@ class RabbitmqDelayedMessageExchange < Formula
   def post_install
     system "rabbitmq-plugins", "enable", "--offline", "rabbitmq_delayed_message_exchange"
   end
+
+  test do
+    ENV["CONF_ENV_FILE"] = "#{HOMEBREW_PREFIX}/etc/rabbitmq/rabbitmq-env.conf"
+    ENV["RABBITMQ_MNESIA_BASE"] = testpath/"var/lib/rabbitmq/mnesia"
+
+    pid = fork { exec "#{HOMEBREW_PREFIX}/sbin/rabbitmq-server" }
+
+    system "#{HOMEBREW_PREFIX}/sbin/rabbitmq-diagnostics", "wait", "--pid", pid
+
+    enabled_plugins = shell_output "rabbitmq-plugins list --enabled"
+    assert_match(/rabbitmq_delayed_message_exchange/, enabled_plugins)
+
+    system "#{HOMEBREW_PREFIX}/sbin/rabbitmqctl", "stop"
+  end
 end
